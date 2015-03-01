e09450f17bd61d1244efe62604fa54ccbdee7253
Sudharsanan <sudhar.ti@gmail.com>
2015-03-01 07:25:12 -0600

    Comit

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -7,8 +7,8 @@ class CustomerServicesController < ApplicationController
       'city' => 'Seattle',
       'state' => 'WA'
     )
-    capability = Twilio::Util::Capability.new ACe7a9cc3dc8168025e510bbb97f392942, 84572ad6e32b5b7894bd9a2cb9da3657
-    capability.allow_client_outgoing APdca1647030733b7c378261b6ae4b646a
+    capability = Twilio::Util::Capability.new 'ACe7a9cc3dc8168025e510bbb97f392942', '84572ad6e32b5b7894bd9a2cb9da3657'
+    capability.allow_client_outgoing 'APdca1647030733b7c378261b6ae4b646a'
     @token = capability.generate()
   end
 end
